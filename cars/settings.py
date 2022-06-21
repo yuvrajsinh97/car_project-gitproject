b266a3322b8fb6ec3366480d1fd895db7813698f
Yuvrajsinh <ryuvrajsinh98@gmail.com>
2022-06-21 18:02:58 +0530

    complated team model

--- cars/settings.py
+++ cars/settings.py
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cars.wsgi.application'
+# WSGI_APPLICATION = 'cars.wsgi.application'
 
 
 # Database
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'cars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cars_db',
+        'USER': 'postgres',
+        'PASSWORD':'bks547897',
+        'HOST':'localhost',
     }
 }
 
@@ -129,3 +132,6 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
